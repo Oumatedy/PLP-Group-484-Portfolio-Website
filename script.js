3c5f98f88a2889587fdf75f9cb23545fdc4d6310
raykaris <ryanmuthoni050@gmail.com>
2025-04-27 07:37:12 -0400

    typed.js update

--- script.js
+++ script.js
@@ -28,12 +28,3 @@ function setTheme(isDark) {
       const isDark = JSON.parse(localStorage.getItem('darkMode'));
       setTheme(isDark);
     });
-
-//typed js
-const typed = new Typed('.multi-text', {
-    strings: ['Web Developer', 'App Developer', 'Photographer'],
-    typedSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
- });
